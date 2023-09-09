f072a81957b740ddf682b6dbcdc0014ae4d252ea
Chhaya Sengar <117908142+ChhayaSengar@users.noreply.github.com>
2023-09-09 13:59:52 +0530

    complete task

--- src/component/TicTacToe.js
+++ src/component/TicTacToe.js
@@ -1,105 +1,99 @@
 import React, { useState, useEffect } from 'react';
-import './TicTacToe.css';
+
 
 function TicTacToe() {
-  // State for the game board
+  // Initialize player scores from local storage or default to 0
+  const [blueScore, setBlueScore] = useState(parseInt(localStorage.getItem('blueScore')) || 0);
+  const [redScore, setRedScore] = useState(parseInt(localStorage.getItem('redScore')) || 0);
+
+  // Initialize the game state
   const [board, setBoard] = useState(Array(9).fill(null));
+  const [nextPlayer, setNextPlayer] = useState('blue');
+  const [winner, setWinner] = useState(null);
 
-  // State for the current player (blue or red)
-  const [currentPlayer, setCurrentPlayer] = useState('blue');
+  // Handle click on a cell
+  const handleCellClick = (index) => {
+    if (board[index] || winner) return; // Ignore if cell is already clicked or game is won
 
-  // State for player scores with persistent storage in local storage
-  const [scores, setScores] = useState({
-    blue: localStorage.getItem('blueScore') || 0,
-    red: localStorage.getItem('redScore') || 0,
-  });
-
-  // Save scores to local storage whenever they change
-  useEffect(() => {
-    localStorage.setItem('blueScore', scores.blue);
-    localStorage.setItem('redScore', scores.red);
-  }, [scores]);
-
-  // Function to handle cell clicks
-  const handleClick = (index) => {
-    if (board[index] || calculateWinner(board)) {
-      return;
-    }
-
-    // Create a copy of the current board
     const newBoard = [...board];
-    newBoard[index] = currentPlayer;
+    newBoard[index] = nextPlayer;
     setBoard(newBoard);
 
-    // Check for a winner
-    const winner = calculateWinner(newBoard);
-    if (winner) {
-      // Update scores and reset the board if there's a winner
-      const updatedScores = { ...scores };
-      updatedScores[winner] += 1;
-      setScores(updatedScores);
-      setBoard(Array(9).fill(null));
-    } 
-    else {
-      // Switch players
-      setCurrentPlayer(currentPlayer === 'blue' ? 'red' : 'blue');
+    const newWinner = calculateWinner(newBoard);
+    if (newWinner) {
+      // Update the scores and save to local storage
+      if (newWinner === 'blue') {
+        setBlueScore(blueScore + 1);
+        localStorage.setItem('blueScore', blueScore + 1);
+      } else {
+        setRedScore(redScore + 1);
+        localStorage.setItem('redScore', redScore + 1);
+      }
+      setWinner(newWinner);
+    } else {
+      setNextPlayer(nextPlayer === 'blue' ? 'red' : 'blue');
     }
   };
 
-  // Function to render individual cells
-  const renderCell = (index) => (
-    <div
-      className={`cell ${board[index]}`}
-      onClick={() => handleClick(index)}
-    >
-      {board[index]}
-    </div>
-  );
+  // Calculate the winner
+  const calculateWinner = (squares) => {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
 
-  // Check for a winner
-  const winner = calculateWinner(board);
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
 
-  // Display game status
-  const status = winner ? `Winner: ${winner}` : `Current Player: ${currentPlayer}`;
+    return null;
+  };
+
+  // Reset the game
+  const resetGame = () => {
+    setBoard(Array(9).fill(null));
+    setNextPlayer('blue');
+    setWinner(null);
+  };
+
+  useEffect(() => {
+    // Reset the game when the winner is determined
+    if (winner) {
+      setTimeout(() => resetGame(), 2000);
+    }
+  }, [winner]);
 
   return (
-    <div className="tic-tac-toe">
+    <div className="App">
+      <h1>Tic-Tac-Toe</h1>
       <div className="scores">
-        <div className="blue-score">Blue: {scores.blue}</div>
-        <div className="red-score">Red: {scores.red}</div>
+        <div className="score blue">Blue: {blueScore}</div>
+        <div className="score red">Red: {redScore}</div>
       </div>
-      <div className="status">{status}</div>
       <div className="board">
-        {Array.from({ length: 9 }, (_, index) => renderCell(index))}
+        {board.map((cell, index) => (
+          <div
+            key={index}
+            className={`cell ${cell}`}
+            onClick={() => handleCellClick(index)}
+          >
+            {cell}
+          </div>
+        ))}
       </div>
+      {winner && <div className="winner">Winner: {winner}</div>}
     </div>
   );
-}
-
-
-// Function to calculate the winner
-function calculateWinner(board) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-
-  for (let line of lines) {
-    const [a, b, c] = line; 
-    // this above line- Destructuring assignment is used to extract the three cell indices 
-    // from the current winning combination being checked.
-    if (board[a] && board[a] === board[b] && board[a] === board[c]) {
-      return board[a];
-    }
-  }
-
-  return null; // No winner yet
 }
 
 export default TicTacToe;
+
